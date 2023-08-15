290a98e747303fca0c786c6f47d3ede3f2cfd0d7
Javier Gomez <javier.gov@outlook.es>
2023-08-15 14:37:32 -0600

    Inicio de app

---
+++ src/test/java/xjgv5/tienda_libros/TiendaLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package xjgv5.tienda_libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
